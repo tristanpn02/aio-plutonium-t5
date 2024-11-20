0a635be7981e151891c39bd120d1a8c3396fb3ce
Tristan Andersen <tristanpn02@gmail.com>
2024-11-20 06:40:04 +0000

    Remove killtransmission script

--- killtransmission.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Wait for transmission-cli to finish downloading
-while transmission-remote -l | grep -q "downloading"; do
-  sleep 10
-done
-
-# Stop transmission-cli when done
-transmission-remote -t all --stop
